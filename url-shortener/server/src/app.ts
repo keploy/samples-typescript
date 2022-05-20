76f2ed305e89d71fe901e5b8d69d1eece33bac6d
re-Tick <jain.ritik.1001@gmail.com>
2022-05-20 22:06:46 +0530

    using local typescript-sdk

--- url-shortener/server/src/app.ts
+++ url-shortener/server/src/app.ts
@@ -1,35 +1,45 @@
+// require("typescript-sdk/dist/integrations/express/register")
+// import "typescript-sdk/dist/integrations/express/register";
 import express from "express";
 import config from "config";
 import bodyParser from "body-parser";
 // import cors from "cors";
 import routes from "./routes/routes";
 import db from "./db";
-import middleware from "typescript-sdk/dist/integerations/express"
-import Keploy from "typescript-sdk/dist/src/keploy";
-
-const app = express();
+import Keploy from "typescript-sdk";
+import middleware from "typescript-sdk/dist/integrations/express/middleware"
 
 
-const port = config.get("port") as number;
-// const port = 8080
+console.log(process.env.KEPLOY_MODE)
+async function main() {
+  const app = express();
+  
+  const port = config.get("port") as number;
+  // const port = 8080
+  
+  let keploy = new Keploy({
+    name: "ts-url-shortner",
+    host: "localhost",
+    port: port,
+    delay: 5,
+    timeout: 60,
+    filter: {},
+  }, {
+    licenseKey: "",
+    url: "http://localhost:8081/api",
+  })
+  // parse application/json
+  app.use(bodyParser.json());
+  // @ts-ignore
+  app.use(middleware(() =>  keploy.create().then( x => x)));
+  // keploy.create();
 
-let keploy = new Keploy({
-  name: "ts-url-shortner",
-  host: "0.0.0.0",
-  port: port,
-  delay: 5,
-  timeout: 60,
-  filter: {},
-}, {
-  licenseKey: "",
-  url: "http://localhost:8081/api",
-})
-// parse application/json
-app.use(bodyParser.json());
-// app.use(middleware(keploy));
+   app.listen(port, "0.0.0.0", () => {
+     console.log(`Application listening at http://localhost:${port}`);
+     db();
+     routes(app, keploy);
+   });
+}
 
-app.listen(port, "0.0.0.0", () => {
-  console.log(`Application listening at http://localhost:${port}`);
-  db();
-  routes(app, keploy);
-});
+main().catch(console.log)
+
--- url-shortener/server/src/routes/routes.ts
+++ url-shortener/server/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Express, Request, Response } from "express";
-import middleware from "typescript-sdk/dist/integerations/express";
+// import middleware from "typescript-sdk/dist/integerations/express";
+
 import Keploy from "typescript-sdk/dist/src/keploy";
 import {
   createShortUrl,
@@ -12,12 +13,22 @@ import shortUrlSchema from "../schema/createShortUrl.schema";
 
 function routes(app: Express, keploy: Keploy) {
   app.get("/healthcheck", (req: Request, res: Response) => {
+    res.set('Type', 'foo-bar');
+    console.log(res.getHeaders());
     return res.send("App is healthy");
   });
 
-  app.post("/url", middleware(keploy),validateResourse(shortUrlSchema), createShortUrl);
+  app.post("/url", validateResourse(shortUrlSchema), createShortUrl);
 
   app.get("/:shortId", handleRedirect);
+
+  app.post("/foo/:pokemon/bar/:level", function(req: Request, res: Response){
+    const { pokemon, level } = req.params;
+
+    res.set('Name', 'Michael');
+    res.set('country', 'United States');
+    res.json({pokemon:pokemon,level:level})
+  })
 
   // app.get("/api/url/:shortId", getShortUrl);
 
