b6427950c2da2a175750805d7802b5ed8e602ee9
G0maa <midomaxgomaa@gmail.com>
2023-03-10 07:10:13 +0200

    added assert to request
    Signed-off-by: G0maa <midomaxgomaa@gmail.com>

--- mocking-stubbing/test/test.js
+++ mocking-stubbing/test/test.js
@@ -1,5 +1,8 @@
 const { describe, test, before } = require('mocha');
-var { NewContext } = require('typescript-sdk/dist/mock/mock');
+const assert = require('assert');
+
+const { NewContext } = require('typescript-sdk/dist/mock/mock');
+
 require('typescript-sdk/dist/integrations/node-fetch/require');
 const fetch = require('node-fetch');
 
@@ -9,9 +12,12 @@ describe('Generating of mocks for external dependencies', function () {
     NewContext({ Mode: 'record', Name: 'testing-auto-mocks' });
   });
 
-  test('Generating node-fetch mocks', () => {
-    fetch('https://reqres.in/api/users/2')
-      .then((res) => res.text())
-      .then((text) => rs.send(text));
+  test('Generating node-fetch mocks', async () => {
+    const res = await fetch('https://reqres.in/api/users/2');
+
+    assert.equal(res.status, 200);
+
+    const resBody = JSON.parse(await res.text());
+    console.log(resBody);
   });
 });
