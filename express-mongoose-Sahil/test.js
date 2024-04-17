854b98b898b84d4c8033effd8cb5f516cc4f4227
s2ahil <sahilpradhan411@gmail.com>
2024-04-17 20:47:47 +0530

    added test.js express project
    Signed-off-by: s2ahil <sahilpradhan411@gmail.com>

---
+++ express-mongoose-Sahil/test.js
@@ -0,0 +1,108 @@
+// This is an application to create online courses also with that you can update,delete and view your courses
+
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const bodyParser=require('body-parser')
+const cors=require('cors')
+const app = express();
+
+
+dotenv.config(); 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+
+
+
+// MongoDB connection
+mongoose.connect(process.env.mongodb_url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// schema of our db
+const Course = mongoose.model("Course", {
+  title: String,
+  description: String,
+  price: Number,
+  published: Boolean,
+});
+
+
+
+// Create a course 
+app.post("/courses", async (req, res) => {
+
+
+
+  try {
+    const course = new Course(req.body);
+    await course.save();
+    res.json({ message: "Course created successfully", courseId: course._id });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+
+
+});
+
+// Read all courses
+app.get("/courses", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.json({ courses });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Read a course by ID
+app.get("/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findById(req.params.id);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ course });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+// Update a course
+app.put("/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ message: "Course updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a course
+app.delete("/courses/:id", async (req, res) => {
+  try {
+    const course = await Course.findByIdAndDelete(req.params.id);
+    if (!course) {
+      return res.status(404).json({ message: "Course not found" });
+    }
+    res.json({ message: "Course deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
