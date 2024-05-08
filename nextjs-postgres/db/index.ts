36e069910f6e9f12f8cbc7788d78b426498ef7c1
Animesh Pathak <kurosakiichigo.songoku@gmail.com>
2024-05-08 11:33:52 +0530

    fix: docker volume for table creation
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>

--- nextjs-postgres/db/index.ts
+++ nextjs-postgres/db/index.ts
@@ -7,40 +7,9 @@ import * as schema from '../schemas/user';
 const client = postgres(process.env.DATABASE_URL ?? "")
 const db = drizzle(client,{schema})
 
-// const migrationClient = postgres('postgresql://postgres:password@localhost:5432/postgres', { max: 1 });
-// migrate(drizzle(migrationClient),{ migrationsFolder: "./drizzle" })
+const migrationClient = postgres('postgresql://postgres:password@localhost:5432/postgres', { max: 1 });
+migrate(drizzle(migrationClient),{ migrationsFolder: "./drizzle" })
 
-// console.log("Migrations Run SuccessFully")
+console.log("Migrations Run SuccessFully")
 
 export default db
-// import {drizzle } from "drizzle-orm/postgres-js"
-// import postgres from "postgres"
-
-
-// const client = postgres(process.env.DATABASE_URL ?? "")
-// const db = drizzle(client)
-
-// export default db
-
-// // import { sql } from "@vercel/postgres";
-// import postgres from "postgres";
-// import {
-//   drizzle as VercelDrizzle,
-//   type VercelPgDatabase,
-// } from "drizzle-orm/vercel-postgres";
-// import {
-//   drizzle as LocalDrizzle,
-//   type PostgresJsDatabase,
-// } from "drizzle-orm/postgres-js";
-
-// let db:
-//   | VercelPgDatabase<Record<string, never>>
-//   | PostgresJsDatabase<Record<string, never>>;
-// if (process.env.NODE_ENV === "production") {
-//   db = VercelDrizzle(sql);
-// } else {
-//   const migrationClient = postgres(process.env.POSTGRES_URL as string);
-//   db = LocalDrizzle(migrationClient);
-// }
-
-// export default db ;
