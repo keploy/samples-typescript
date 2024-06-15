fca44682407a9ea47a4c368ad549f1042c316c08
Animesh Pathak <53110238+Sonichigo@users.noreply.github.com>
2024-06-15 21:54:52 +0530

    feat: add utg guide (#49)
    * feat: add utg guide
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    * feat: add utg guide
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    * fix: keploy install guide
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    * fix: keploy install guide
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    * fix: keploy install guide
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>
    
    ---------
    
    Signed-off-by: Animesh Pathak <kurosakiichigo.songoku@gmail.com>

--- express-mongoose/test/routes.test.js
+++ express-mongoose/test/routes.test.js
@@ -39,4 +39,67 @@ describe('GET /student/:name', () => {
     expect(response.status).toBe(400);
     expect(response.text).toBe(`Failed to fetch student data as Error: ${errorMessage}`);
   });
+
+it('should return a list of all students', async () => {
+    const mockStudents = [
+      { name: 'John', email: 'john@example.com' },
+      { name: 'Jane', email: 'jane@example.com' },
+    ];
+  
+    Student.find.mockResolvedValue(mockStudents);
+  
+    const response = await request(app).get('/students');
+  
+    expect(response.status).toBe(200);
+    expect(response.body).toEqual(mockStudents);
+  });
+
+
+it('should return a 400 status and error message when an error occurs', async () => {
+    const errorMessage = 'Database error';
+    Student.find.mockRejectedValue(new Error(errorMessage));
+  
+    const response = await request(app).get('/students');
+  
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(`Failed to fetch student data as Error: ${errorMessage}`);
+  });
+
+
+it('should successfully register a new student', async () => {
+    const newStudent = { name: 'John', email: 'john@example.com' };
+  
+    Student.prototype.save.mockResolvedValue(newStudent);
+  
+    const response = await request(app).post('/students').send(newStudent);
+  
+    expect(response.status).toBe(201);
+    expect(response.text).toBe('Student registration successful!');
+  });
+
+
+it('should update student details', async () => {
+    const updatedStudent = { name: 'John', email: 'john.updated@example.com' };
+    const studentId = '12345';
+  
+    Student.findByIdAndUpdate.mockResolvedValue(updatedStudent);
+  
+    const response = await request(app).patch(`/student/${studentId}`).send(updatedStudent);
+  
+    expect(response.status).toBe(200);
+    expect(response.text).toBe(`Student detail updated to \n ${updatedStudent}`);
+  });
+
+
+it('should return a 400 status and error message when registration fails', async () => {
+    const newStudent = { name: 'John', email: 'john@example.com' };
+    const errorMessage = 'Registration error';
+    Student.prototype.save.mockRejectedValue(new Error(errorMessage));
+  
+    const response = await request(app).post('/students').send(newStudent);
+  
+    expect(response.status).toBe(400);
+    expect(response.text).toBe(`Failed to register Student as Error: ${errorMessage}`);
+  });
+
 });
