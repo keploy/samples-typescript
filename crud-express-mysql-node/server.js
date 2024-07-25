995b4dfd8640ed0d676a96690d071356e6de3ad7
Yaten Dhingra <yaten598@gmail.com>
2024-07-25 10:20:44 +0530

    added get and insert request functions

--- crud-express-mysql-node/server.js
+++ crud-express-mysql-node/server.js
@@ -15,10 +15,10 @@ const app = express();
 
 // ENV variables for initialising database.
 const dbConfig = {
-  host: process.env.MYSQL_HOST || "localhost",
-  user: process.env.MYSQL_USER || "user",
-  password: process.env.MYSQL_PASSWORD || "password",
-  database: process.env.MYSQL_DATABASE || "mysql_db",
+  host: process.env.MYSQL_HOST || "localhost", // default host is set to 'localhost'
+  user: process.env.MYSQL_USER || "user", // default user is set to 'user'
+  password: process.env.MYSQL_PASSWORD || "password", // default password is set to 'password'
+  database: process.env.MYSQL_DATABASE || "mysql_db", // default database is set to 'mysql_db'
 };
 
 // Initialise MySQL
@@ -32,7 +32,10 @@ connection.connect((err) => {
 
 // Route for getting all the items from DB.
 app.get("/get", (req, res) => {
-  res.send("this route will manage get requests from the mysql database.");
+  connection.query(`SELECT * FROM my_table`, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
 });
 
 // Route for updating items in DB.
@@ -47,9 +50,25 @@ app.delete("/delete/:id", (req, res) => {
 
 // Route for adding items to DB.
 app.post("/create", (req, res) => {
-  res.send("this route will manage create requests from the mysql database.");
+  const { name, age } = req.body;
+
+  // Check if all the params are given or not, if not, then return error.
+  if (!name || !age) {
+    return res.status(400).json({ message: "Name and age are required" });
+  }
+
+  // Insert all the values in the table if all params are provided.
+  connection.query(
+    `INSERT INTO my_table (name, age) VALUES(?, ?)`,
+    [name, age],
+    (err, result) => {
+      if (err) throw err;
+      res.json({ message: "Values inserted successfully.", result });
+    }
+  );
 });
 
+// Default port is '3000' unless specified in the `.env` file.
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
