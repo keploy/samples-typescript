d8376ba54ebc7e19b87bd4661c1f806b6248ec87
Yash Khare <yash2010118@akgec.ac.in>
2024-09-18 23:46:30 +0530

    Revert "fix: standardize apps (#63)" (#64)
    This reverts commit b499be5d755d5ea12d2e47f144526e2cc49ba270.

--- node-mongoose/tests/app.test.js
+++ node-mongoose/tests/app.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const { app, mongoConnection ,Course} = require('./app');
+const { app, mongoConnection ,Course} = require('../app');
 const mongoose = require("mongoose");
 
 beforeAll(async () => {
