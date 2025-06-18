53176417fe157cfbc8e7be9b0d548eed7289cf6f
Kaushik Verma <kaushikverma321@gmail.com>
2025-06-18 22:10:36 +0530

    Enhance CRUD operations with JOI validation, input sanitization, auth middleware, and rate limiting

--- express-mongodb/app.js
+++ express-mongodb/app.js
@@ -1,15 +1,15 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
-const mongoose = require("mongoose");
-const Product = require("./models/product.model.js");
 const productRoute = require("./routes/product.route.js");
-const connectDB = require('./db/connect');
+const connectDB = require("./db/connect");
 const app = express();
+const { generalRateLimiter } = require("./middlewares/rateLimiter.middleware");
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
+app.use(generalRateLimiter);
 
 // routes
 app.use("/api/products", productRoute);
@@ -18,6 +18,6 @@ app.get("/", (req, res) => {
   res.send("Hello from Node API Server Updated");
 });
 
-// connectDB(process.env.MONGODB_URL);
+connectDB(process.env.MONGODB_URL);
 
 module.exports = app;
